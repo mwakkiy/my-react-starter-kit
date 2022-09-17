57d928d9d96f9a44cc0d83993b3622fad1acaf2f
mwakkiy <wakkiy@gmail.com>
2022-09-17 14:09:15 +0900

    1st commit

---
+++ README.md
@@ -0,0 +1,19 @@
+
+# インストール
+```
+npm install
+```
+
+# 開始
+```
+npm start
+```
+
+# メモ
+```
+npx create-react-app my-app --template redux
+cd my-app
+npm install @reduxjs/toolkit
+```
+
+
---
+++ src/views/pages/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import TodoList from '../components/TodoList';
+// import AddTodo from '../components/AddTodo';
+
+const Main = () => {
+    return (
+        <div>
+            aaaa
+        </div>
+    );
+};
+
+export default Main;
